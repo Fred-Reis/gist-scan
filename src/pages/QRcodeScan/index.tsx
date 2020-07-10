5274a57c2155e52135776334011eeb6db09d4a2a
Fred-Reis <fredgost@hotmail.com>
2020-07-09 21:49:55 -0300

    feat: qrcode page done

--- src/pages/QRcodeScan/index.tsx
+++ src/pages/QRcodeScan/index.tsx
@@ -1,55 +1,70 @@
-import React, { useCallback } from 'react';
-import { Text, Button } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { Button, Text, Image, ActivityIndicator, View } from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
-import { useNavigation } from '@react-navigation/native';
+import Lottie from 'lottie-react-native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-// import { Container } from './styles';
+import { Container, Title, HomeButton } from './styles';
+
+import icon from '../../assets/icon.png';
+import qrcodeImg from '../../assets/qr-code-scan.json';
 
 const QRcodeScan: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [isFocused, setIsFocused] = useState<boolean>(false);
   const navigation = useNavigation();
-
-  const handleScanQRcode = useCallback((e: any) => {
-    console.log('QR code scanned!', e);
-    // navigation.navigate('GistPage');
-  }, []);
   let scanner: any;
 
-  const startScan = () => {
-    if (scanner) {
-      scanner._setScanning(false);
-    }
-  };
+  useFocusEffect(
+    useCallback(() => {
+      setIsFocused(true);
+
+      return () => setIsFocused(false);
+    }, []),
+  );
+
+  const handleScanQRcode = useCallback(
+    (e: any): void => {
+      navigation.navigate('GistPage', { data: e.data });
+    },
+    [navigation],
+  );
 
   return (
-    <>
-      <QRCodeScanner
-        ref={(camera) => (scanner = camera)}
-        onRead={handleScanQRcode}
-        // reactivate={true}
-        // reactivateTimeout={3000}
-        showMarker={true}
-        markerStyle={{ borderColor: '#e5e5e5' }}
-        cameraStyle={{
-          overflow: 'hidden',
-          position: 'absolute',
-          height: 400,
-          width: 400,
-        }}
-        bottomContent={
-          <Button title="Reactivate" onPress={() => startScan()} />
-        }
-        // topContent={
-        //   <Text style={{ color: '#e5e5e5', fontSize: 50, marginTop: 10 }}>
-        //     Qualquer coisa
-        //   </Text>
-        // }
-        // bottomContent={
-        //   <Text style={{ color: '#e5e5e5', fontSize: 40, fontWeight: 'bold' }}>
-        //     Point the scanner at qrcode
-        //   </Text>
-        // }
-      />
-    </>
+    <Container>
+      {isFocused ? (
+        <>
+          <QRCodeScanner
+            ref={(camera) => (scanner = camera)}
+            onRead={handleScanQRcode}
+            showMarker={true}
+            markerStyle={{ borderColor: '#e5e5e5' }}
+            cameraStyle={{
+              overflow: 'hidden',
+              position: 'absolute',
+              height: 400,
+              width: 400,
+            }}
+            bottomContent={<Title>Point the scanner at qrcode</Title>}
+          />
+
+          <Lottie
+            source={qrcodeImg}
+            resizeMode="contain"
+            autoPlay
+            loop
+            style={{ marginBottom: 50, marginTop: 20, height: 200 }}
+          />
+
+          <HomeButton onPress={() => navigation.navigate('Home')}>
+            <Icon name="home-circle" color="#e5e5e5" size={40} />
+          </HomeButton>
+        </>
+      ) : (
+        <View />
+      )}
+    </Container>
   );
 };
 
